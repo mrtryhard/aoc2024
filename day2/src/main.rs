d7390d1a5037760e41dbcf8e8f86f7596ff8eae8
Alexandre Leblanc <alexandre.leblanc@broadsign.com>
2024-12-03 10:57:00 +0900

    Prepare Day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -14,54 +14,11 @@ fn main() {
 
 /// Main body for solving
 fn solve(input: &str) -> String {
-    let (mut left_list, mut right_list) = parse_lists(input);
-
-    left_list.sort();
-    right_list.sort();
-
-    let total: i32 = zip(left_list, right_list)
-        .map(|(x, y)| x.max(y) - x.min(y))
-        .sum();
-
-    total.to_string()
+    "".to_string()
 }
 
 fn solve_complex(input: &str) -> String {
-    let (left_list, right_list) = parse_lists(input);
-
-    let similarities = left_list
-        .iter()
-        .map(|x| right_list.iter().filter(|y| *y == x).count() as i32)
-        .collect::<Vec<i32>>();
-
-    let total: i32 = zip(left_list, similarities).map(|(x, y)| x * y).sum();
-
-    total.to_string()
-}
-
-fn parse_lists(input: &str) -> (Vec<i32>, Vec<i32>) {
-    let left_list: Vec<i32> = input
-        .split("\n")
-        .map(|str| {
-            str.split_whitespace()
-                .next()
-                .unwrap()
-                .parse::<i32>()
-                .unwrap()
-        })
-        .collect();
-
-    let right_list: Vec<i32> = input
-        .split("\n")
-        .map(|str| {
-            str.split_whitespace()
-                .nth(1)
-                .unwrap()
-                .parse::<i32>()
-                .unwrap()
-        })
-        .collect();
-    (left_list, right_list)
+    "".to_string()
 }
 
 #[cfg(test)]
